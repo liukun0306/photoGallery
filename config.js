d9a2854db2b98890bb9ba565ac3f4a486f9469b4
liukun0306 <77088175+liukun0306@users.noreply.github.com>
2024-02-21 11:05:54 +0800

    Upload by PicGo

---
+++ config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author       : laofan
+ * @Date         : 2023-02-24 11:20:15
+ * @LastEditors: liukun
+ * @LastEditTime: 2023-12-14 10:05:39
+ * @Description  : 全局路径配置文件
+ */
+const configUrl = {
+    baseURL: 'http://192.168.2.26:8080', //资源服务接口地址
+    // baseURL: 'http://localhost:8080', //资源服务接口地址
+    processorTreeUrl:'http://localhost:9010',
+    wizardExeURL: 'http://localhost:8070',
+    wizardExePath: 'D:\\install\\Notepad++\\notepad++.exe',
+    txtFilePath: 'C:\\Users\\cq\\Downloads'
+}
